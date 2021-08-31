6ee81f0e16bf9ee25ea8beef615305a1fd2606c9
Karen Gonzalez <karen@Karens-MacBook-Pro.local>
2021-08-30 22:04:02 -0500

    Initial commit

---
+++ backend-modules/src/main/java/com/mi/vecino/backendmodules/BackendModulesApplication.java
@@ -0,0 +1,13 @@
+package com.mi.vecino.backendmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendModulesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendModulesApplication.class, args);
+  }
+
+}
---
+++ backend-modules/src/test/java/com/mi/vecino/backendmodules/BackendModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi.vecino.backendmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendModulesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
